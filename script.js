a7976712dfe08ebc86464e07f5954939564b7efe
zirris512 <zirris512@gmail.com>
2023-01-28 21:37:25 -0600

    add fetch functionality to unsplash api

---
+++ script.js
@@ -0,0 +1,68 @@
+const imageContainer = document.querySelector("#image-container");
+const loader = document.querySelector("#loader");
+
+let photosArr = [];
+
+// Unsplash API
+const count = 10;
+const apiKey = "";
+const apiUrl = `https://api.unsplash.com/photos/random?count=${count}&client_id=${apiKey}`;
+
+// Helper function to set attributes
+function setAttributes(el, attrs) {
+    for (const key in attrs) {
+        el[key] = attrs[key];
+    }
+}
+// Display images on page
+function displayImages() {
+    photosArr.forEach((photo) => {
+        const item = document.createElement("a");
+        setAttributes(item, {
+            href: photo.links.html,
+            target: "_blank",
+        });
+
+        const image = document.createElement("img");
+        setAttributes(image, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+
+        item.append(image);
+        imageContainer.append(item);
+    });
+}
+
+// Get photos from Unsplash
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            const error = new Error(`Could not fetch images: ${response.status}`);
+            error.errors = response.errors;
+            throw error;
+        }
+        photosArr = await response.json();
+        displayImages();
+        console.log(photosArr);
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(
+                err.message,
+                err.errors.map((errorMsg) => `${errorMsg}, `)
+            );
+        }
+    }
+}
+
+// Check if scrolling near the bottom, Load more photos
+window.addEventListener("scroll", (e) => {
+    if (innerHeight + scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos();
+    }
+});
+
+// getPhotos();
